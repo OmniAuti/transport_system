f6b2c283b668603e0ed8f01ae763fa7bb6a96574
OmniOmni <austin.olijar@gmail.com>
2024-08-07 08:08:55 -0500

    rider added

--- app/controllers/single.php
+++ app/controllers/single.php
@@ -5,16 +5,18 @@ $result;
 $header;
 
 if (isset($_GET['pi'])) {
-    $sql = "SELECT pickup_time FROM schedule WHERE schedule_id = :id";    
+    $sql = "SELECT * FROM schedule WHERE schedule_id = :id";    
     $result = $db->queryOne($sql, ['id' => $_GET['pi']]);
     $schedule = $result['pickup_time'];
     $header = "Pickup";
 } else if (isset($_GET['di'])) {
-    $sql = "SELECT dropoff_time FROM schedule WHERE schedule_id = :id";    
+    $sql = "SELECT * FROM schedule WHERE schedule_id = :id";    
     $result = $db->queryOne($sql, ['id' => $_GET['di']]);
     $schedule = $result['dropoff_time'];
     $header = "Dropoff";
 }
 
-view('single.view.php', ['schedule' => $schedule, 'header' => $header]);
+$user = $db->queryOne('SELECT * FROM users WHERE userID = :userID', ['userID' => $result['userID']]);
+
+view('single.view.php', ['schedule' => $schedule, 'header' => $header, 'user' => $user]);
 exit();
--- views/single.view.php
+++ views/single.view.php
@@ -4,10 +4,12 @@
     $date = substr($schedule, 0, $t);
     $time = substr($schedule, $t + 1, strlen($schedule));
 ?>
+
 <h1><?= $header ?></h1>
 
 <div>
     <p>Date: <?= $date ?></p>
     <p>Time: <?= $time ?></p>
+    <p>Passenger: <?= $user['username'] ?></p>
     <p>Assigned Driver: </p>
 </div>
