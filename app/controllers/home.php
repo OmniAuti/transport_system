484e9cd9304b267b07bf45c9b3f2a33dececad0b
OmniOmni <austin.olijar@gmail.com>
2024-08-10 12:55:44 -0500

    employee dashboard;

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -10,11 +10,11 @@ if (isset($_SESSION['customer'])) {
     $schedule = $db->queryAll($sql, ['customer_id' => $customer_id]);
     view('home-customer.view.php', ['schedule' => $schedule]);
 } else if (isset($_SESSION['employee'])) {
-    echo 'employee';
-    dd($_SESSION['employee']);
-
+    $id = $_SESSION['employee']['id'];
+    $sql = "SELECT * FROM schedule WHERE assigned_employee = :assigned_employee";
+    $schedule = $db->queryAll($sql, ['assigned_employee' => $id]);
+    view('home-employee.view.php', ['schedule' => $schedule]);
 } else if (empty($_SESSION)) {
     dd('empty');
-
 }
 exit();
--- views/home-employee.view.php
+++ views/home-employee.view.php
@@ -2,13 +2,38 @@
     view('partials/header.partial.php', ['title' => 'Home']);
 ?>
 <body>
-    <h1>Home</h1>
+    <h1>Employee Home</h1>
     <?php
     if (isset($_SESSION['user']['name'])) {
         echo($_SESSION['user']['name']);
     }
 
+    if (isset($_SESSION['employee']['name'])) {
+        echo('Hello, '.$_SESSION['employee']['name'].'. Here are your scheduled jobs.');
+    }
+    ?>
+    <p>Pickups</p>
+    <?php
     // NEEDS ASSIGNED PICKUPS/DROPOFFS
+    foreach($schedule as $jobs):
+        // need to include customer
+    ?>
+    <div>
+        <p><?= $jobs['pickup_time'] ?></p>
+    </div>
+    <?php
+    endforeach;
+    ?>
+    <p>Dropoffs</p>
+    <?php
+    // NEEDS ASSIGNED PICKUPS/DROPOFFS
+    foreach($schedule as $jobs):
+    ?>
+    <div>
+        <p><?= $jobs['dropoff_time'] ?></p>
+    </div>
+    <?php
+    endforeach;
     ?>
    
 </body>
